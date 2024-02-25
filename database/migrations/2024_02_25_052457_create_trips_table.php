92078cdd05eca155dd8ba99102bf70cfa265e962
dreyliba <libaandrey@gmail.com>
2024-02-25 13:58:43 +0800

    Setup model and migration

---
+++ database/migrations/2024_02_25_052457_create_trips_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('boat_id')->constrained();
+            $table->date('date');
+            $table->time('departure_time');
+            $table->time('return_time');
+            $table->integer('available_seats')->default(30)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
---
+++ database/migrations/2024_02_25_052458_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trip_id')->constrained();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->integer('number_of_passenger');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
